bbff6534e933cdaafb34a51de40a5b571bc8d523
okada <atunori06@gmail.com>
2021-01-09 19:59:09 +0900

    api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="container" class="jsfiddle-container">
+  <form autocomplete="nope">
+
+    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+      <input class="mdl-textfield__input" type="text" id="zip"  autocomplete="nope">
+      <label class="mdl-textfield__label" for="zip" >郵便番号を入力</label>
+    </div>
+
+    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+      <input class="mdl-textfield__input" type="text" id="state" autocomplete="nope">
+      <label class="mdl-textfield__label" for="state">都道府県</label>
+    </div>
+
+    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+      <input class="mdl-textfield__input" type="text" id="city" autocomplete="nope">
+      <label class="mdl-textfield__label" for="city">市区町村</label>
+    </div>
+
+    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+      <input class="mdl-textfield__input" type="text" id="street" autocomplete="nope">
+      <label class="mdl-textfield__label" for="street">町域</label>
+    </div>
+
+
+  </form>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  // APIキーを指定して住所補完サービスを作成
+  var api = new postcodejp.address.AutoComplementService('JSROs8akSxHGYQ4KL4Tc9CXY2pXTc0dXrAay9kG');
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+// 郵便番号テキストボックスを指定
+api.setZipTextbox('zip')
+
+// 住所補完フィールドを追加
+api.add(new postcodejp.address.StateTextbox('state'));
+api.add(new postcodejp.address.TownTextbox('city'));
+api.add(new postcodejp.address.StreetTextbox('street'));
+
+
+api.setAdditionalCallback(function(data) {
+  mdlCleanUp();
+});
+// 郵便番号テキストボックスの監視を開始
+api.observe();
+
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
