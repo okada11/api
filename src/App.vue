e9260acab3a0ac43499780b009ff5fa4d9f8cff1
okada <atunori06@gmail.com>
2021-01-10 16:09:18 +0900

    api

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,35 @@
 <template>
-  <div id="container" class="jsfiddle-container">
-  <form autocomplete="nope">
-
-    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-      <input class="mdl-textfield__input" type="text" id="zip"  autocomplete="nope">
-      <label class="mdl-textfield__label" for="zip" >郵便番号を入力</label>
+  <div class="about">
+    <div class="weather">
+      <input type="text" v-model="postcode">
+      <button @click="getData">住所入力</button>
+      <p>Adress:{{adress}}</p>
     </div>
-
-    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-      <input class="mdl-textfield__input" type="text" id="state" autocomplete="nope">
-      <label class="mdl-textfield__label" for="state">都道府県</label>
-    </div>
-
-    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-      <input class="mdl-textfield__input" type="text" id="city" autocomplete="nope">
-      <label class="mdl-textfield__label" for="city">市区町村</label>
-    </div>
-
-    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-      <input class="mdl-textfield__input" type="text" id="street" autocomplete="nope">
-      <label class="mdl-textfield__label" for="street">町域</label>
-    </div>
-
-
-  </form>
-</div>
+  </div>
 </template>
 
 <script>
-  // APIキーを指定して住所補完サービスを作成
-  var api = new postcodejp.address.AutoComplementService('JSROs8akSxHGYQ4KL4Tc9CXY2pXTc0dXrAay9kG');
+import axios from "axios";
+export default {
+  props: ["city"],
+  data() {
+    return {
+      postcode: '',
+      adress: ''
+    };
+  },
+  methods: {
+    async getData(){
+      const item = await axios.get(
+        `https://apis.postcode-jp.com/api/v4/postcodes/${this.postcode}?apiKey=JSROs8akSxHGYQ4KL4Tc9CXY2pXTc0dXrAay9kG`
+      );
+      // レスポンス（受け取った情報）の中から住所を取得
+      // レスポンス内容確認
+      console.log(item);
 
-// 郵便番号テキストボックスを指定
-api.setZipTextbox('zip')
-
-// 住所補完フィールドを追加
-api.add(new postcodejp.address.StateTextbox('state'));
-api.add(new postcodejp.address.TownTextbox('city'));
-api.add(new postcodejp.address.StreetTextbox('street'));
-
-
-api.setAdditionalCallback(function(data) {
-  mdlCleanUp();
-});
-// 郵便番号テキストボックスの監視を開始
-api.observe();
-
+      // 住所代入
+      // this.adress = 住所
+    }
+  }
+};
 </script>
